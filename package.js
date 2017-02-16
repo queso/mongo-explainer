5bcf4bf9608bf3ce9e3547b18d0e66659e78c168
Josh Owens <joshua.owens@gmail.com>
2017-02-16 17:05:54 -0500

    Tweak names to match

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'joshowens:mongo-explainer',
-  version: '0.1.0',
+  version: '0.1.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Learn which mongo queries need indexed.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/queso/mongo-explainer',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md',
@@ -15,12 +15,12 @@ Package.onUse(function(api) {
   api.versionsFrom('1.4.2.3');
   api.use('ecmascript');
   api.use('mongo');
-  api.mainModule('mongo-explain.js');
+  api.mainModule('mongo-explainer.js');
 });
 
 Package.onTest(function(api) {
   api.use('ecmascript');
   api.use('tinytest');
-  api.use('mongo-explain');
-  api.mainModule('mongo-explain-tests.js');
+  api.use('joshowens:mongo-explainer');
+  api.mainModule('mongo-explainer-tests.js');
 });
